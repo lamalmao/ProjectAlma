45d3dd14dbd59f4ce159c23855020d9e767800c9
akimcom2 <akimcom2@gmail.com>
2022-10-21 22:37:46 +0300

    mailsucksesfull

--- controllers/garbage/test_controller.js
+++ controllers/garbage/test_controller.js
@@ -1,3 +1,25 @@
+const nodemailer = require('nodemailer');
 module.exports = function(req, res) {
-	res.end('Garbage test');
+	let mailTransporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		auth: {
+			user: 'sendernodejs@gmail.com',
+			pass: 'iwovimbhzkficdaq'
+		}
+	});
+
+	let mailDetails = {
+		from: 'sendernodejs@gmail.com',
+		to: 'akimcom2@gmail.com',
+		subject: 'Password replace',
+		text: 'Тестовое сообщение'
+	};
+
+	mailTransporter.sendMail(mailDetails, function(err, data) {
+		if(err) {
+			console.log('Error');
+		} else {
+			console.log('Email sent successfully');
+		}
+	});
 };
--- garbage/nodemailer.js
+++ garbage/nodemailer.js
@@ -1,27 +1,4 @@
 const nodemailer = require('nodemailer')
 app.get('/newpass', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`);
 
-    let mailTransporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        auth: {
-            user: 'sendernodejs@gmail.com',
-            pass: 'iwovimbhzkficdaq'
-        }
-    });
-
-    let mailDetails = {
-        from: 'sendernodejs@gmail.com',
-        to: mail,
-        subject: 'Password replace',
-        text: 'Ваш новый пароль-57493984'
-    };
-
-    mailTransporter.sendMail(mailDetails, function(err, data) {
-        if(err) {
-            console.log('Error');
-        } else {
-            console.log('Email sent successfully');
-        }
-    });
 });
