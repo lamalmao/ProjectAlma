87e3ac85f846a632430daf56d68c1e02e9d3b6ff
lamalmao <laesia>
2022-10-21 20:31:50 +0300

    Модели

---
+++ models/users.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const User = new Schema({
+	signID: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	role: {
+		type: String,
+		required: true
+		// student, teacher, staff, administrator
+	},
+	login: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		hash: String,
+		salt: String,
+		// required: true
+	},
+	fullname: {
+		type: String,
+		required: true,
+		default: 'NONE'
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	phone: {
+		type: String,
+		unique: true
+	},
+	group: String,
+	groupList: Array,
+	profession: String
+});
+
+module.exports = model('users', User);
