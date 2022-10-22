89177a428eff6f8340f4629f0d5054ba739f3a3f
akimcom2 <akimcom2@gmail.com>
2022-10-22 14:40:08 +0300

    Отправка сообщений

--- tools/emailer.js
+++ tools/emailer.js
@@ -1,1 +1,25 @@
-const nodemailer= require("nodemailer")
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+
+module.exports = async function(target, reason='new') {
+    let token = crypto.randomBytes(3).toString('hex');
+
+    let mailTransporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        auth: {
+            user: 'sendernodejs@gmail.com',
+            pass: 'iwovimbhzkficdaq'
+        }
+    });
+
+    let mailDetails = {
+        from: 'sendernodejs@gmail.com',
+        to: target,
+        subject: 'Password replace',
+        text: `https://laesia.site/check?q=${token}&r=${reason}`,
+    };
+
+    let result = await mailTransporter.sendMail(mailDetails);
+    console.log(result);
+    return token;
+};
