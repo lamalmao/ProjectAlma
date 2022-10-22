2e505963d34c32dc9af2c1134321db8c2c92be45
lamalmao <laesia>
2022-10-22 22:41:41 +0300

    urlencoded

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const HTTPServer = http.createServer(app);
 // Подключение pug движка
 app.set('view engine', 'pug');
 // Подключение парсера тела сообщения
-app.use(express.urlencoded( { extented: true } ));
+app.use(express.urlencoded( { extended: true } ));
 app.use(express.json());
 app.use(cookieParser());
 
